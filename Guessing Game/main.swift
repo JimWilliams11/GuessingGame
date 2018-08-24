3b30d5db0281ef9227815e73b8e0abeba9516876
Jim Williams <jimwilliams@macbook-pro-7.local>
2018-08-24 12:35:13 -0700

    Guessing Game

--- Guessing Game/main.swift
+++ Guessing Game/main.swift
@@ -2,11 +2,103 @@
 //  main.swift
 //  Guessing Game
 //
-//  Created by Jim Williams on 8/16/18.
-//  Copyright © 2018 Jim Williams. All rights reserved.
+//  Created by James D Williams on 8/16/18.
+//  Copyright © 2018 James D Williams. All rights reserved.
 //
 
 import Foundation
 
-print("Hello, World!")
+
+// Guessing Game Code Starts Below
+
+var playAgain = false
+
+repeat {
+    
+    var numberOfTries = 5
+    
+    var guessedNumber: Int? = nil
+    
+    let randomNumber = Int(arc4random_uniform(100))+1
+    
+    print("Please guess a number between 1 and 100!")
+    
+    repeat {
+        repeat {
+            let input = readLine()!
+            
+            if let inputAsNumber = Int(input) {
+                guessedNumber = inputAsNumber
+            } else {
+                print("Please enter a number")
+                guessedNumber = nil
+            }
+        } while guessedNumber == nil
+        
+        
+        guard let guessedNumber = guessedNumber else {
+            fatalError()
+        }
+        if guessedNumber == randomNumber {
+            print("You got it right!")
+            break
+        } else if guessedNumber > randomNumber {
+            print("Please guess lower.")
+            numberOfTries -= 1
+        } else {
+            print("Please guess higher.")
+            numberOfTries -= 1
+        }
+        print("You have \(numberOfTries) tries left")
+        
+        if numberOfTries == 0 {
+            print("You've lost. The correct number was \(randomNumber)")
+        }
+    } while numberOfTries > 0
+    
+    
+    print("Would you like to play again? Yes: Y or No: N")
+    
+    let input = readLine()!
+    
+    if input.lowercased() == "y" {
+        playAgain = true
+    } else {
+        playAgain = false
+    }
+    
+} while playAgain
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
